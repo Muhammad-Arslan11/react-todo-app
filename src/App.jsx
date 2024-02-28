36e0c70c4d569b2d408ab9989341805fc686abef
Muhammad-Arslan11 <113223888+Muhammad-Arslan11@users.noreply.github.com>
2024-02-28 14:56:15 +0500

    Update App.jsx
    There was a logical error in the update function of App.jsx which was fixed

--- src/App.jsx
+++ src/App.jsx
@@ -15,10 +15,7 @@ function App() {
 
   const updateTodo = (id, todo) => {
     setTodos((prev) =>
-      prev.map((prevTodo) => {
-        if (prevTodo.id === id) todo;
-        prevTodo;
-      })
+      prev.map((prevTodo) => prevTodo.id === id ? todo : prevTodo)
     );
   };
 
