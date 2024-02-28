a8417c3691d052f92da2cb5ad55d21e11bf10c74
Muhammad-Arslan11 <113223888+Muhammad-Arslan11@users.noreply.github.com>
2024-02-28 14:56:48 +0500

    Update TodoItem.jsx
    there was a typo in the file which is fixed now

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -9,7 +9,7 @@ function TodoItem({ todo }) {
 
     const editTodo = () =>{
         updateTodo(todo.id, {...todo, todo: todoMsg})
-        setIsTodoEditableisTodoEditble(false)
+        setIsTodoEditable(false)
         }
         
         const toggleCompleted = () =>{
